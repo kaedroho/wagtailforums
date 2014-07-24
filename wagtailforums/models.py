f76ec646e3a3b4ec704d797da11e5731326b1633
Karl Hobley <karlhobley10@gmail.com>
2014-07-24 20:46:06 +0100

    Added permissions

--- wagtailforums/models.py
+++ wagtailforums/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import PermissionDenied
 from django import forms
 
 from wagtail.wagtailcore.models import Page
@@ -49,6 +50,36 @@ class BaseForumReply(BaseForumPost):
     def delete_url(self):
         return self.url + 'delete/'
 
+    def user_can_edit(self, user):
+        # Check if the user has permission to do this in wagtail
+        if self.permissions_for_user(user).can_edit():
+            return True
+
+        # Make sure the user is active
+        if not user.is_active:
+            return False
+
+        # Owner can edit
+        if user == self.owner:
+            return True
+
+        return False
+
+    def user_can_delete(self, user):
+        # Check if the user has permission to do this in wagtail
+        if self.permissions_for_user(user).can_delete():
+            return True
+
+        # Make sure the user is active
+        if not user.is_active:
+            return False
+
+        # Owner can delete
+        if user == self.owner:
+            return True
+
+        return False
+
     def route(self, request, path_components):
         if self.live:
             if path_components == ['edit']:
@@ -59,7 +90,19 @@ class BaseForumReply(BaseForumPost):
 
         return super(ForumReply, self).route(request, path_components)
 
+    def get_context(self, request):
+        context = super(BaseForumReply, self).get_context(request)
+        context['user_can_edit'] = self.user_can_edit(request.user)
+        context['user_can_delete'] = self.user_can_delete(request.user)
+        return context
+
     def edit_view(self, request):
+        if not self.user_can_edit(request.user):
+            if request.method == 'POST':
+                raise PermissionDenied
+            else:
+                return redirect(self.url)
+
         form = self.get_form_class()(request.POST or None, request.FILES or None, instance=self)
 
         if form.is_valid():
@@ -74,6 +117,12 @@ class BaseForumReply(BaseForumPost):
             return render(request, get_template_name(self.template, 'edit'), context)
 
     def delete_view(self, request):
+        if not self.user_can_delete(request.user):
+            if request.method == 'POST':
+                raise PermissionDenied
+            else:
+                return redirect(self.url)
+
         if request.method == 'POST':
             self.live = False
             self.save()
@@ -117,6 +166,66 @@ class BaseForumTopic(BaseForumPost):
     def get_all_replies(self):
         return get_replies().descendant_of(self).live()
 
+    def user_can_post_reply(self, user):
+        # If theres no reply model, no replies can be created
+        if not self.reply_model:
+            return False
+
+        # Check if the user has permission to do this in wagtail
+        if self.permissions_for_user(user).can_add_subpage():
+            return True
+
+        # Make sure the user is active
+        if not user.is_active:
+            return False
+
+        return True
+
+    def user_can_publish_reply(self, user):
+        # Users need to be able to post replies before they can publish them
+        if not self.user_can_post_reply(user):
+            return False
+
+        # Check if the user has permission to do this in wagtail
+        if self.permissions_for_user(user).can_publish_subpage():
+            return True
+
+        # Make sure the user is active
+        if not user.is_active:
+            return False
+
+        return True
+
+    def user_can_edit(self, user):
+        # Check if the user has permission to do this in wagtail
+        if self.permissions_for_user(user).can_edit():
+            return True
+
+        # Make sure the user is active
+        if not user.is_active:
+            return False
+
+        # Owner can edit
+        if user == self.owner:
+            return True
+
+        return False
+
+    def user_can_delete(self, user):
+        # Check if the user has permission to do this in wagtail
+        if self.permissions_for_user(user).can_delete():
+            return True
+
+        # Make sure the user is active
+        if not user.is_active:
+            return False
+
+        # Owner can delete
+        if user == self.owner:
+            return True
+
+        return False
+
     @property
     def edit_url(self):
         return self.url + 'edit/'
@@ -135,6 +244,14 @@ class BaseForumTopic(BaseForumPost):
 
         return super(BaseForumTopic, self).route(request, path_components)
 
+    def get_context(self, request):
+        context = super(BaseForumTopic, self).get_context(request)
+        context['user_can_post_reply'] = self.user_can_post_reply(request.user)
+        context['user_can_publish_reply'] = self.user_can_publish_reply(request.user)
+        context['user_can_edit'] = self.user_can_edit(request.user)
+        context['user_can_delete'] = self.user_can_delete(request.user)
+        return context
+
     def main_view(self, request):
         form = self.reply_model.get_form_class()(request.POST or None, request.FILES or None)
 
@@ -151,6 +268,12 @@ class BaseForumTopic(BaseForumPost):
             return render(request, self.get_template(request), context)
 
     def edit_view(self, request):
+        if not self.user_can_edit(request.user):
+            if request.method == 'POST':
+                raise PermissionDenied
+            else:
+                return redirect(self.url)
+
         form = self.get_form_class()(request.POST or None, request.FILES or None, instance=self)
 
         if form.is_valid():
@@ -165,6 +288,12 @@ class BaseForumTopic(BaseForumPost):
             return render(request, get_template_name(self.template, 'edit'), context)
 
     def delete_view(self, request):
+        if not self.user_can_delete(request.user):
+            if request.method == 'POST':
+                raise PermissionDenied
+            else:
+                return redirect(self.url)
+
         if request.method == 'POST':
             self.live = False
             self.save()
@@ -209,6 +338,36 @@ class BaseForumIndex(Page):
     def get_all_replies(self):
         return get_replies().descendant_of(self).live()
 
+    def user_can_post_topic(self, user):
+        # If theres no topic model, no topics can be created
+        if not self.topic_model:
+            return False
+
+        # Check if the user has permission to do this in wagtail
+        if self.permissions_for_user(user).can_add_subpage():
+            return True
+
+        # Make sure the user is active
+        if not user.is_active:
+            return False
+
+        return True
+
+    def user_can_publish_topic(self, user):
+        # Users need to be able to post topics before they can publish them
+        if not self.user_can_post_topic(user):
+            return False
+
+        # Check if the user has permission to do this in wagtail
+        if self.permissions_for_user(user).can_publish_subpage():
+            return True
+
+        # Make sure the user is active
+        if not user.is_active:
+            return False
+
+        return True
+
     @property
     def search_url(self):
         return self.url + 'search/'
@@ -219,6 +378,12 @@ class BaseForumIndex(Page):
                 return RouteResult(self, kwargs=dict(action='search'))
 
         return super(BaseForumIndex, self).route(request, path_components)
+
+    def get_context(self, request):
+        context = super(BaseForumIndex, self).get_context(request)
+        context['user_can_post_topic'] = self.user_can_post_topic(request.user)
+        context['user_can_publish_topic'] = self.user_can_publish_topic(request.user)
+        return context
 
     def search_view(self, request):
         if 'q' in request.GET:
